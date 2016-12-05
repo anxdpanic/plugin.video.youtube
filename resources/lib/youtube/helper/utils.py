725089ebc78a521baf1d3ff4dfb14952df6a00bf
Jeffrey Frasure <jdf@sierra.local>
2016-12-05 09:30:07 -0500

    Updates

--- resources/lib/youtube/helper/utils.py
+++ resources/lib/youtube/helper/utils.py
@@ -178,7 +178,7 @@ def update_video_infos(provider, context, video_id_dict, playlist_item_id_dict=N
         video_item.set_use_dash(context.get_settings().use_dash())
 
         # set mediatype
-        video_item.set_mediatype('episode')  # using episode since all setContent is currently episode as well
+        video_item.set_mediatype('video')  # using video
 
         # set the title
         video_item.set_title(snippet['title'])
