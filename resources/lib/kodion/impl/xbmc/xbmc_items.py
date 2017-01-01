5a033537c6a36261ded30c165672c6c6b9bf7dd6
guidosarducci <guidosarducci@users.noreply.github.com>
2016-12-31 18:53:04 -0800

    Revert SPMC MPEG-DASH support broken due to maintainer changes
    This reverts commit efd079b64e8443597165186877b25e007d14464e.

--- resources/lib/kodion/impl/xbmc/xbmc_items.py
+++ resources/lib/kodion/impl/xbmc/xbmc_items.py
@@ -26,7 +26,7 @@ def to_video_item(context, video_item):
         item.addContextMenuItems(video_item.get_context_menu(), replaceItems=video_item.replace_context_menu())
         pass
 
-    if video_item.use_dash() and settings.dash_support_addon():
+    if video_item.use_dash():
         item.setProperty('inputstreamaddon', 'inputstream.adaptive')
         item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
 
--- resources/lib/kodion/impl/xbmc/xbmc_system_version.py
+++ resources/lib/kodion/impl/xbmc/xbmc_system_version.py
@@ -6,40 +6,39 @@ from ..abstract_system_version import AbstractSystemVersion
 
 
 class XbmcSystemVersion(AbstractSystemVersion):
-    def __init__(self, version, releasename, appname):
-        super(XbmcSystemVersion, self).__init__(version, releasename, appname)
+    def __init__(self, version, name):
+        super(XbmcSystemVersion, self).__init__(version, name)
         try:
             json_query = xbmc.executeJSONRPC(
                 '{ "jsonrpc": "2.0", "method": "Application.GetProperties", "params": {"properties": ["version", "name"]}, "id": 1 }')
             json_query = unicode(json_query, 'utf-8', errors='ignore')
             json_query = json.loads(json_query)
             version_installed = []
-            version_installed = json_query['result']['version']
-            self._version = (version_installed.get('major', 1), version_installed.get('minor', 0))
-            self._appname = json_query['result']['name']
-            pass
+            if 'result' in json_query and 'version' in json_query['result']:
+                version_installed = json_query['result']['version']
+                self._version = (version_installed.get('major', 1), version_installed.get('minor', 0))
+                pass
         except:
             self._version = (1, 0)  # Frodo
-            self._appname = 'Unknown Application'
             pass
-        self._releasename = 'Unknown XBMC Release'
+        self._name = 'Unknown XBMC System'
         if self._version >= (12, 0):
-            self._releasename = 'Frodo'
+            self._name = 'Frodo'
             pass
         if self._version >= (13, 0):
-            self._releasename = 'Gotham'
+            self._name = 'Gotham'
             pass
         if self._version >= (14, 0):
-            self._releasename = 'Helix'
+            self._name = 'Helix'
             pass
         if self._version >= (15, 0):
-            self._releasename = 'Isengard'
+            self._name = 'Isengard'
             pass
         if self._version >= (16, 0):
-            self._releasename = 'Jarvis'
+            self._name = 'Jarvis'
             pass
         if self._version >= (17, 0):
-            self._releasename = 'Krypton'
+            self._name = 'Krypton'
             pass
 
     pass
