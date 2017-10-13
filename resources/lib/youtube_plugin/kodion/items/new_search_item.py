91323f383db8f98175eaf1de1f51fa57f3f6fb91
anxdpanic <anxdpanic@users.noreply.github.com>
2017-10-13 16:27:56 -0400

    quick search and quick search (incognito) to Settings -> Folders

--- resources/lib/youtube_plugin/kodion/items/new_search_item.py
+++ resources/lib/youtube_plugin/kodion/items/new_search_item.py
@@ -5,7 +5,7 @@ from .. import constants
 
 
 class NewSearchItem(DirectoryItem):
-    def __init__(self, context, alt_name=None, image=None, fanart=None):
+    def __init__(self, context, alt_name=None, image=None, fanart=None, use_history=True):
         name = alt_name
         if not name:
             name = '[B]' + context.localize(constants.localize.SEARCH_NEW) + '[/B]'
@@ -15,7 +15,9 @@ class NewSearchItem(DirectoryItem):
             image = context.create_resource_path('media/new_search.png')
             pass
 
-        DirectoryItem.__init__(self, name, context.create_uri([constants.paths.SEARCH, 'input']), image=image)
+        use_history = str(use_history).lower()
+
+        DirectoryItem.__init__(self, name, context.create_uri([constants.paths.SEARCH, 'input'], params={'use_history': use_history}), image=image)
         if fanart:
             self.set_fanart(fanart)
             pass
