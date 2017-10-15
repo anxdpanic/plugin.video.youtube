d73e499fee8d65752b36f0b35a8408d1bbfc60ce
anxdpanic <anxdpanic@users.noreply.github.com>
2017-10-15 09:27:26 -0400

    [login client] update auth urls/endpoints and headers (based on yt api guides/auth/devices), rename methods

--- resources/lib/youtube_plugin/youtube/client/login_client.py
+++ resources/lib/youtube_plugin/youtube/client/login_client.py
@@ -54,21 +54,15 @@ class LoginClient(object):
             print text
 
     def revoke(self, refresh_token):
-        headers = {'Host': 'www.youtube.com',
-                   'Connection': 'keep-alive',
-                   'Origin': 'https://www.youtube.com',
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.28 Safari/537.36',
-                   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                   'Accept': '*/*',
-                   'DNT': '1',
-                   'Referer': 'https://www.youtube.com/tv',
-                   'Accept-Encoding': 'gzip, deflate',
-                   'Accept-Language': 'en-US,en;q=0.8,de;q=0.6'}
+        # https://developers.google.com/youtube/v3/guides/auth/devices
+        headers = {'Host': 'accounts.google.com',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+                   'Content-Type': 'application/x-www-form-urlencoded'}
 
         post_data = {'token': refresh_token}
 
         # url
-        url = 'https://www.youtube.com/o/oauth2/revoke'
+        url = 'https://accounts.google.com/o/oauth2/revoke'
 
         result = requests.post(url, data=post_data, headers=headers, verify=self._verify)
 
@@ -92,16 +86,10 @@ class LoginClient(object):
                                   client_secret=client_secret, grant_type=grant_type)
 
     def refresh_token(self, refresh_token, client_id='', client_secret='', grant_type=''):
-        headers = {'Host': 'www.youtube.com',
-                   'Connection': 'keep-alive',
-                   'Origin': 'https://www.youtube.com',
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.28 Safari/537.36',
-                   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                   'Accept': '*/*',
-                   'DNT': '1',
-                   'Referer': 'https://www.youtube.com/tv',
-                   'Accept-Encoding': 'gzip, deflate',
-                   'Accept-Language': 'en-US,en;q=0.8,de;q=0.6'}
+        # https://developers.google.com/youtube/v3/guides/auth/devices
+        headers = {'Host': 'www.googleapis.com',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+                   'Content-Type': 'application/x-www-form-urlencoded'}
 
         client_id = client_id or self._config['id']
         client_secret = client_secret or self._config['secret']
@@ -112,7 +100,7 @@ class LoginClient(object):
                      'grant_type': 'refresh_token'}
 
         # url
-        url = 'https://www.youtube.com/o/oauth2/token'
+        url = 'https://www.googleapis.com/oauth2/v4/token'
 
         config_type = self._get_config_type(client_id, client_secret)
         context.log_debug('Refresh token: Config: |%s| Client id [:5]: |%s| Client secret [:5]: |%s|' %
@@ -143,23 +131,16 @@ class LoginClient(object):
 
         return '', ''
 
-    def get_device_token_tv(self, code, client_id='', client_secret='', grant_type=''):
+    def request_access_token_tv(self, code, client_id='', client_secret='', grant_type=''):
         client_id = client_id or self.CONFIGS['youtube-tv']['id']
         client_secret = client_secret or self.CONFIGS['youtube-tv']['secret']
-        return self.get_device_token(code, client_id=client_id, client_secret=client_secret, grant_type=grant_type)
+        return self.request_access_token(code, client_id=client_id, client_secret=client_secret, grant_type=grant_type)
 
-    def get_device_token(self, code, client_id='', client_secret='', grant_type=''):
-        headers = {'Host': 'www.youtube.com',
-                   'Connection': 'keep-alive',
-                   'Origin': 'https://www.youtube.com',
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.28 Safari/537.36',
-                   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                   'Accept': '*/*',
-                   'DNT': '1',
-                   'Referer': 'https://www.youtube.com/tv',
-                   'Accept-Encoding': 'gzip, deflate',
-                   'Accept-Language': 'en-US,en;q=0.8,de;q=0.6',
-                   'If-Match': '*'}
+    def request_access_token(self, code, client_id='', client_secret='', grant_type=''):
+        # https://developers.google.com/youtube/v3/guides/auth/devices
+        headers = {'Host': 'www.googleapis.com',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+                   'Content-Type': 'application/x-www-form-urlencoded'}
 
         client_id = client_id or self._config['id']
         client_secret = client_secret or self._config['secret']
@@ -170,26 +151,29 @@ class LoginClient(object):
                      'grant_type': 'http://oauth.net/grant_type/device/1.0'}
 
         # url
-        url = 'https://www.youtube.com/o/oauth2/token'
+        url = 'https://www.googleapis.com/oauth2/v4/token'
 
         config_type = self._get_config_type(client_id, client_secret)
-        context.log_debug('Retrieving device token: Config: |%s| Client id [:5]: |%s| Client secret [:5]: |%s|' %
+        context.log_debug('Requesting access token: Config: |%s| Client id [:5]: |%s| Client secret [:5]: |%s|' %
                           (config_type, client_id[:5], client_secret[:5]))
 
         result = requests.post(url, data=post_data, headers=headers, verify=self._verify)
 
+        authorization_pending = False
         try:
             json_data = result.json()
             if 'error' in json_data:
                 if json_data['error'] != u'authorization_pending':
-                    context.log_error('Retrieving device token: Code: |%s| JSON: |%s|' % (str(result.status_code), json_data))
+                    context.log_error('Requesting access token: Code: |%s| JSON: |%s|' % (str(result.status_code), json_data))
                     raise LoginException(json_data['error'])
+                else:
+                    authorization_pending = True
         except ValueError:
             json_data = None
 
-        if result.status_code != requests.codes.ok:
+        if (result.status_code != requests.codes.ok) and not authorization_pending:
             response_dump = self._get_response_dump(result, json_data)
-            context.log_error('Retrieving device token: Config: |%s| Client id [:5]: |%s| Client secret [:5]: |%s| Code: |%s| Response dump |%s|' %
+            context.log_error('Requesting access token: Config: |%s| Client id [:5]: |%s| Client secret [:5]: |%s| Code: |%s| Response dump |%s|' %
                               (config_type, client_id[:5], client_secret[:5], str(result.status_code), response_dump))
             raise LoginException('Login Failed: Code %s' % str(result.status_code))
 
@@ -198,37 +182,32 @@ class LoginClient(object):
                 return json_data
             else:
                 return result.json()
+        else:
+            response_dump = self._get_response_dump(result, json_data)
+            context.log_error('Requesting access token: Config: |%s| Client id [:5]: |%s| Client secret [:5]: |%s| Code: |%s| Response dump |%s|' %
+                              (config_type, client_id[:5], client_secret[:5], str(result.status_code), response_dump))
+            raise LoginException('Login Failed: Unknown response')
 
-        return None
-
-    def generate_user_code_tv(self):
+    def request_device_and_user_code_tv(self):
         client_id = str(self.CONFIGS['youtube-tv']['id'])
-        return self.generate_user_code(client_id=client_id)
+        return self.request_device_and_user_code(client_id=client_id)
 
-    def generate_user_code(self, client_id=''):
-        headers = {'Host': 'www.youtube.com',
-                   'Connection': 'keep-alive',
-                   'Origin': 'https://www.youtube.com',
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.28 Safari/537.36',
-                   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                   'Accept': '*/*',
-                   'DNT': '1',
-                   'Referer': 'https://www.youtube.com/tv',
-                   'Accept-Encoding': 'gzip, deflate',
-                   'Accept-Language': 'en-US,en;q=0.8,de;q=0.6',
-                   'If-Match': '*'}
+    def request_device_and_user_code(self, client_id=''):
+        # https://developers.google.com/youtube/v3/guides/auth/devices
+        headers = {'Host': 'accounts.google.com',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+                   'Content-Type': 'application/x-www-form-urlencoded'}
 
         client_id = client_id or self._config['id']
 
         post_data = {'client_id': client_id,
                      'scope': 'https://www.googleapis.com/auth/youtube'}
-        # 'scope': 'http://gdata.youtube.com https://www.googleapis.com/auth/youtube-paid-content'}
 
         # url
-        url = 'https://www.youtube.com/o/oauth2/device/code'
+        url = 'https://accounts.google.com/o/oauth2/device/code'
 
         config_type = self._get_config_type(client_id)
-        context.log_debug('Generating user code: Config: |%s| Client id [:5]: |%s|' %
+        context.log_debug('Requesting device and user code: Config: |%s| Client id [:5]: |%s|' %
                           (config_type, client_id[:5]))
 
         result = requests.post(url, data=post_data, headers=headers, verify=self._verify)
@@ -236,14 +215,14 @@ class LoginClient(object):
         try:
             json_data = result.json()
             if 'error' in json_data:
-                context.log_error('Generate user code failed: Code: |%s| JSON: |%s|' % (str(result.status_code), json_data))
+                context.log_error('Requesting device and user code failed: Code: |%s| JSON: |%s|' % (str(result.status_code), json_data))
                 raise LoginException(json_data['error'])
         except ValueError:
             json_data = None
 
         if result.status_code != requests.codes.ok:
             response_dump = self._get_response_dump(result, json_data)
-            context.log_error('Generate user code failed: Config: |%s| Client id [:5]: |%s| Code: |%s| Response dump |%s|' %
+            context.log_error('Requesting device and user code failed: Config: |%s| Client id [:5]: |%s| Code: |%s| Response dump |%s|' %
                               (config_type, client_id[:5], str(result.status_code), response_dump))
             raise LoginException('Login Failed')
 
@@ -252,8 +231,11 @@ class LoginClient(object):
                 return json_data
             else:
                 return result.json()
-
-        return None
+        else:
+            response_dump = self._get_response_dump(result, json_data)
+            context.log_error('Requesting access token: Config: |%s| Client id [:5]: |%s| Code: |%s| Response dump |%s|' %
+                              (config_type, client_id[:5], str(result.status_code), response_dump))
+            raise LoginException('Login Failed: Unknown response')
 
     def get_access_token(self):
         return self._access_token
