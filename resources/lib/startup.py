e5c151976565a79ceabac1519ed0fbc0ae30008a
anxdpanic <anxdpanic@users.noreply.github.com>
2018-01-17 09:24:35 -0500

    Dash no longer supported in SPMC
    - does not play current manifest

--- resources/lib/startup.py
+++ resources/lib/startup.py
@@ -11,23 +11,18 @@ context = Context(plugin_id='plugin.video.youtube')
 
 context.log_debug('YouTube settings startup initialization...')
 version = context.get_system_version().get_version()
-application = context.get_system_version().get_app_name()
 settings = context.get_settings()
 
 mpd_addon = False
-mpd_builtin = False
 
 if version >= (17, 0):
     mpd_addon = True
-elif version >= (16, 5) and application == 'SPMC':
-    mpd_builtin = True
 else:
     settings.set_bool(setting.USE_DASH, False)
 
-settings.set_bool(setting.DASH_SUPPORT_BUILTIN, mpd_builtin)
 settings.set_bool(setting.DASH_SUPPORT_ADDON, mpd_addon)
-context.log_notice('Startup: detected {0}, setting DASH_SUPPORT_BUILTIN = {1}, DASH_SUPPORT_ADDON = {2}'
-                   .format(context.get_system_version(), mpd_builtin, mpd_addon))
+context.log_notice('Startup: detected {0}, DASH_SUPPORT_ADDON = {1}'
+                   .format(context.get_system_version(), mpd_addon))
 
 
 def strptime(stamp, stamp_fmt):
@@ -61,8 +56,7 @@ ping_delay_time = 60
 ping_timestamp = None
 first_run = True
 
-
-if mpd_addon or mpd_builtin:
+if mpd_addon:
     monitor = Monitor()
     while not monitor.abortRequested():
 
