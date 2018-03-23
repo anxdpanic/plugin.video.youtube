6b030229752aec06a96e8edd33ae08432e59e296
anxdpanic <anxdpanic@users.noreply.github.com>
2018-03-23 11:53:07 -0400

    provide path and params logging at runtime

--- resources/lib/youtube_plugin/kodion/runner.py
+++ resources/lib/youtube_plugin/kodion/runner.py
@@ -27,8 +27,6 @@ def run(provider, context=None):
     version = context.get_system_version()
     name = context.get_name()
     addon_version = context.get_version()
-    context.log_notice('Running: %s (%s) on %s with %s' % (name, addon_version, version, python_version))
-    context.log_debug('Path: "%s' % context.get_path())
     redacted = '<redacted>'
     context_params = copy.deepcopy(context.get_params())
     if 'api_key' in context_params:
@@ -37,7 +35,11 @@ def run(provider, context=None):
         context_params['client_id'] = redacted
     if 'client_secret' in context_params:
         context_params['client_secret'] = redacted
-    context.log_debug('Params: "%s"' % str(context_params))
+
+    context.log_notice('Running: %s (%s) on %s with %s\n\tPath: %s\n\tParams: %s' %
+                       (name, addon_version, version, python_version,
+                        context.get_path(), str(context_params)))
+
     __RUNNER__.run(provider, context)
     provider.tear_down(context)
     context.log_debug('Shutdown of Kodion')
