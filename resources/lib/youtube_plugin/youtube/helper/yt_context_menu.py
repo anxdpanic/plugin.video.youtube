6c10410672b148ee1d7e7ed9e455306a616a1f13
anxdpanic <anxdpanic@users.noreply.github.com>
2018-06-07 10:35:35 -0400

    [K18] fix context menu - More - Related Videos

--- resources/lib/youtube_plugin/youtube/helper/yt_context_menu.py
+++ resources/lib/youtube_plugin/youtube/helper/yt_context_menu.py
@@ -13,10 +13,10 @@ def append_more_for_video(context_menu, provider, context, video_id, is_logged_i
         _refresh_container = '1'
 
     context_menu.append((context.localize(provider.LOCAL_MAP['youtube.video.more']),
-                         'Container.Update(%s)' % context.create_uri(['video', 'more'],
-                                                                     {'video_id': video_id,
-                                                                      'logged_in': _is_logged_in,
-                                                                      'refresh_container': _refresh_container})))
+                         'RunPlugin(%s)' % context.create_uri(['video', 'more'],
+                                                              {'video_id': video_id,
+                                                               'logged_in': _is_logged_in,
+                                                               'refresh_container': _refresh_container})))
 
 
 def append_content_from_description(context_menu, provider, context, video_id):
