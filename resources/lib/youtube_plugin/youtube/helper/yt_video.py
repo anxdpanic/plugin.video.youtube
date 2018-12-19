3edced3b55c64353e6155d30e7fcb92db833c66a
anxdpanic <anxdpanic@users.noreply.github.com>
2018-12-19 14:15:22 -0500

    allow rating from a custom keymap
    - RunPlugin(plugin://plugin.video.youtube/video/rate/?rating=like)
    - RunPlugin(plugin://plugin.video.youtube/video/rate/?rating=dislike)
    - RunPlugin(plugin://plugin.video.youtube/video/rate/?rating=none)

--- resources/lib/youtube_plugin/youtube/helper/yt_video.py
+++ resources/lib/youtube_plugin/youtube/helper/yt_video.py
@@ -8,17 +8,39 @@
     See LICENSES/GPL-2.0-only for more information.
 """
 
+import re
+
+import xbmc
+
 from ... import kodion
 from ...youtube.helper import v3
 
 
 def _process_rate_video(provider, context, re_match):
+    ratings = ['like', 'dislike', 'none']
+
+    rating_param = context.get_param('rating', '')
+    if rating_param:
+        rating_param = rating_param.lower()
+
     video_id = context.get_param('video_id', '')
     if not video_id:
         try:
             video_id = re_match.group('video_id')
         except IndexError:
-            raise kodion.KodionException('video/rate/: missing video_id')
+            if rating_param in ratings:
+                listitem_path = xbmc.getInfoLabel('Container.ListItem(0).FileNameAndPath')
+                if listitem_path.startswith('plugin://%s/play/' % context._plugin_id):
+                    match = re.search(r'.*video_id=(?P<video_id>[a-zA-Z0-9_\-]{11}).*', listitem_path)
+                    if match:
+                        video_id = match.group('video_id')
+
+            if not video_id:
+                raise kodion.KodionException('video/rate/: missing video_id')
+
+            if not rating_param:
+                raise kodion.KodionException('video/rate/: missing rating')
+
     try:
         current_rating = re_match.group('rating')
     except IndexError:
@@ -34,12 +56,18 @@ def _process_rate_video(provider, context, re_match):
         if items:
             current_rating = items[0].get('rating', '')
 
-    ratings = ['like', 'dislike', 'none']
     rating_items = []
-    for rating in ratings:
-        if rating != current_rating:
-            rating_items.append((context.localize(provider.LOCAL_MAP['youtube.video.rate.%s' % rating]), rating))
-    result = context.get_ui().on_select(context.localize(provider.LOCAL_MAP['youtube.video.rate']), rating_items)
+    if not rating_param:
+        for rating in ratings:
+            if rating != current_rating:
+                rating_items.append((context.localize(provider.LOCAL_MAP['youtube.video.rate.%s' % rating]), rating))
+        result = context.get_ui().on_select(context.localize(provider.LOCAL_MAP['youtube.video.rate']), rating_items)
+    else:
+        if rating_param != current_rating:
+            result = rating_param
+        else:
+            result = -1
+
     if result != -1:
         client = provider.get_client(context).rate_video(video_id, result)
 
