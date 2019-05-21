f60b05e288cea59e13ed7bc630f491de9fbf8d3f
anxdpanic <anxdpanic@gmail.com>
2019-05-21 14:26:00 -0400

    uniform quotations

--- resources/lib/youtube_plugin/kodion/utils/http_server.py
+++ resources/lib/youtube_plugin/kodion/utils/http_server.py
@@ -67,7 +67,7 @@ class YouTubeRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'application/json; charset=utf-8')
             self.send_header('Content-Length', len(client_json))
             self.end_headers()
-            self.wfile.write(client_json.encode("utf-8"))
+            self.wfile.write(client_json.encode('utf-8'))
 
         logger.log_debug('HTTPServer: Request uri path |{proxy_path}|'.format(proxy_path=self.path))
 
