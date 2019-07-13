0fac5d29a998b46f90dfac8f44f49ffd87a32a50
anxdpanic <anxdpanic@gmail.com>
2019-07-13 11:28:17 -0400

    Update C years

--- resources/lib/youtube_requests.py
+++ resources/lib/youtube_requests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 
-    Copyright (C) 2017-2018 plugin.video.youtube
+    Copyright (C) 2017-2019 plugin.video.youtube
 
     SPDX-License-Identifier: GPL-2.0-only
     See LICENSES/GPL-2.0-only for more information.
