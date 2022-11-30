31907d6f5051268e87ce19737c998b8f5c1bce29
anxdpanic <anxdpanic@users.noreply.github.com>
2022-11-29 19:25:43 -0500

    tags whitespace and default strings

--- resources/lib/youtube_plugin/youtube/helper/tags.py
+++ resources/lib/youtube_plugin/youtube/helper/tags.py
@@ -7,21 +7,21 @@
     SPDX-License-Identifier: GPL-2.0-only
     See LICENSES/GPL-2.0-only for more information.
 """
-#from ... import kodion
-#from ...youtube.helper import v3
+
 import xbmc
 import xbmcaddon
 import os
 import sqlite3
 
-addonID=xbmcaddon.Addon().getAddonInfo("id")
-db_dir = xbmc.translatePath("special://profile/addon_data/"+addonID)
+addonID = xbmcaddon.Addon().getAddonInfo("id")
+db_dir = xbmc.translatePath("special://profile/addon_data/" + addonID)
 db_path = os.path.join(db_dir, 'tags.db')
 
-db=sqlite3.connect(db_path)
+db = sqlite3.connect(db_path)
+
 
 def init():
-    #import web_pdb; web_pdb.set_trace()
+    # import web_pdb; web_pdb.set_trace()
     with db:
         cur = db.cursor()
         cur.execute("begin")
@@ -31,124 +31,137 @@ def init():
         db.commit()
         cur.close()
     return
-    
+
+
 def create_channel(channel_id, title, item_uri, image, kind):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("INSERT OR REPLACE INTO channels(channel,title,uri,image,kind) VALUES (?,?,?,?,?);",(channel_id, title, item_uri, image, kind))
+    cur.execute("INSERT OR REPLACE INTO channels(channel,title,uri,image,kind) VALUES (?,?,?,?,?);", (channel_id, title, item_uri, image, kind))
     db.commit()
     cur.close()
+
 
 def create_tag(tag_id, title):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("INSERT OR REPLACE INTO tags(tag,title) VALUES (?,?);",(tag_id, title))
+    cur.execute("INSERT OR REPLACE INTO tags(tag,title) VALUES (?,?);", (tag_id, title))
     db.commit()
     cur.close()
+
 
 def add_channel_tag(channel_id, tag):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("INSERT OR REPLACE INTO channel_tag(tag,channel) VALUES (?,?);",(tag, channel_id))
+    cur.execute("INSERT OR REPLACE INTO channel_tag(tag,channel) VALUES (?,?);", (tag, channel_id))
     db.commit()
     cur.close()
 
+
 def get_channels(tag):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    if len(tag)>0:
-        cur.execute("select channels.channel, channels.title, channels.uri, channels.image from channels join channel_tag on channel_tag.channel = channels.channel where channel_tag.tag = ?;",(tag,))
+    if len(tag) > 0:
+        cur.execute("select channels.channel, channels.title, channels.uri, channels.image from channels join channel_tag on channel_tag.channel = channels.channel where channel_tag.tag = ?;", (tag,))
     else:
         cur.execute("select channels.channel, channels.title, channels.uri, channels.image from channels;")
     rows = cur.fetchall()
     cur.close()
-    channels=[]
+    channels = []
     for i in range(len(rows)):
-        channels.append([rows[i][0],rows[i][1],rows[i][2],rows[i][3]])
+        channels.append([rows[i][0], rows[i][1], rows[i][2], rows[i][3]])
     return channels
 
+
 def get_empty_channels():
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
     cur.execute("select channels.channel, channels.title, channels.uri, channels.image from channels left join channel_tag on channel_tag.channel = channels.channel where channel_tag.tag is NULL;")
     rows = cur.fetchall()
     cur.close()
-    channels=[]
+    channels = []
     for i in range(len(rows)):
-        channels.append([rows[i][0],rows[i][1],rows[i][2],rows[i][3]])
+        channels.append([rows[i][0], rows[i][1], rows[i][2], rows[i][3]])
     return channels
 
+
 def get_channel_name(channel_id):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("select title from channels where channel = ?;",(channel_id,))
+    cur.execute("select title from channels where channel = ?;", (channel_id,))
     row = cur.fetchone()
     cur.close()
     return row[0]
+
 
 def get_tag_name(tag):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("select title from tags where tag = ?;",(tag,))
+    cur.execute("select title from tags where tag = ?;", (tag,))
     row = cur.fetchone()
     cur.close()
     return row[0]
 
+
 def get_tags():
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
     cur.execute("SELECT DISTINCT tag, title FROM tags;")
     rows = cur.fetchall()
     cur.close()
-    tags=[]
-    for i in range (len(rows)):
-        tags.append([rows[i][0],rows[i][1]])
+    tags = []
+    for i in range(len(rows)):
+        tags.append([rows[i][0], rows[i][1]])
     return tags
+
 
 def get_tags_add(channel_id):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("SELECT DISTINCT tags.tag, tags.title FROM tags LEFT JOIN channel_tag on channel_tag.tag = tags.tag and channel_tag.channel = ? WHERE channel_tag.tag is NULL;",(channel_id,))
+    cur.execute("SELECT DISTINCT tags.tag, tags.title FROM tags LEFT JOIN channel_tag on channel_tag.tag = tags.tag and channel_tag.channel = ? WHERE channel_tag.tag is NULL;", (channel_id,))
     rows = cur.fetchall()
     cur.close()
-    tags=[]
-    for i in range (len(rows)):
-        tags.append([rows[i][0],rows[i][1]])
+    tags = []
+    for i in range(len(rows)):
+        tags.append([rows[i][0], rows[i][1]])
     return tags
 
+
 def rename_tag(tag_id, new_name):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("UPDATE tags SET title = ? WHERE tag = ?;",(new_name,tag_id))
+    cur.execute("UPDATE tags SET title = ? WHERE tag = ?;", (new_name, tag_id))
     db.commit()
     cur.close()
+
 
 def delete_tag(tag_id):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("DELETE FROM tags WHERE tag = ?;",(tag_id,))
+    cur.execute("DELETE FROM tags WHERE tag = ?;", (tag_id,))
     db.commit()
-    cur.execute("DELETE FROM channel_tag WHERE tag = ?;",(tag_id,))
+    cur.execute("DELETE FROM channel_tag WHERE tag = ?;", (tag_id,))
     db.commit()
     cur.close()
 
+
 def delete_channel_tag(channel_id, tag_id):
-    #init()
+    # init()
     cur = db.cursor()
     cur.execute("begin")
-    cur.execute("DELETE FROM channel_tag WHERE tag = ? and channel = ?;",(tag_id,channel_id))
+    cur.execute("DELETE FROM channel_tag WHERE tag = ? and channel = ?;", (tag_id, channel_id))
     db.commit()
     cur.close()
+
 
 init()
