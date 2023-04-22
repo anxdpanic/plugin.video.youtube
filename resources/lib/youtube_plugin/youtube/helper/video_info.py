0bdf93589917f401dfd9324ad2b9f3bef830e3dd
anxdpanic <anxdpanic@users.noreply.github.com>
2023-04-22 13:05:34 -0400

    no message

--- resources/lib/youtube_plugin/youtube/helper/video_info.py
+++ resources/lib/youtube_plugin/youtube/helper/video_info.py
@@ -764,9 +764,10 @@ class VideoInfo(object):
                 r.raise_for_status()
                 player_response = r.json()
                 if player_response.get('playabilityStatus', {}).get('status', 'OK') in \
-                        ('AGE_CHECK_REQUIRED', 'UNPLAYABLE', 'CONTENT_CHECK_REQUIRED') and attempt == 0:
-                    payload['context']['client']['clientName'] = 'ANDROID_EMBEDDED_PLAYER'
-                    continue
+                        ('AGE_CHECK_REQUIRED', 'UNPLAYABLE', 'CONTENT_CHECK_REQUIRED'):
+                    if attempt == 0:
+                        payload['context']['client']['clientName'] = 'ANDROID_EMBEDDED_PLAYER'
+                        continue
             except:
                 error_message = 'Failed to get player response for video_id "%s"' % video_id
                 self._context.log_error(error_message + '\n' + traceback.format_exc())
