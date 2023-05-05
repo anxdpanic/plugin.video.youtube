65be3443d6d1a138e5effa238ccae08317d703e5
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2023-05-05 15:28:50 +1000

    Tidy up

--- resources/lib/youtube_plugin/youtube/helper/video_info.py
+++ resources/lib/youtube_plugin/youtube/helper/video_info.py
@@ -1224,7 +1224,6 @@ class VideoInfo(object):
                          is_httpd_live(port=self._context.get_settings().httpd_port()))
 
         s_info = {}
-
         adaptive_fmts = streaming_data.get('adaptiveFormats', [])
         std_fmts = streaming_data.get('formats', [])
         mpd_url = streaming_data.get('dashManifestUrl') or \
@@ -1361,11 +1360,11 @@ class VideoInfo(object):
         for stream_map in adaptive_fmts:
             mime_type = stream_map.get('mimeType')
             if not mime_type:
-                 continue
+                continue
 
             itag = str(stream_map.get('itag', ''))
             if not itag:
-                 continue
+                continue
 
             index_range = stream_map.get('indexRange')
             if not index_range:
@@ -1373,7 +1372,7 @@ class VideoInfo(object):
 
             init_range = stream_map.get('initRange')
             if not init_range:
-                 continue
+                continue
 
             url = stream_map.get('url')
             if not url and self._cipher and 'signatureCipher' in stream_map:
