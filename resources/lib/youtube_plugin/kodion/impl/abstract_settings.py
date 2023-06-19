530027a55e137f6af1585e60b6815c2524e385fe
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2023-06-19 23:51:45 +1000

    Improve stream quality and features selection

--- resources/lib/youtube_plugin/kodion/impl/abstract_settings.py
+++ resources/lib/youtube_plugin/kodion/impl/abstract_settings.py
@@ -141,20 +141,19 @@ class AbstractSettings(object):
             return self.get_bool(constants.setting.MPD_VIDEOS, False)
         return False
 
-    def include_hdr(self):
-        return self.get_bool(constants.setting.DASH_INCL_HDR, False)
+    _LIVE_STREAM_TYPES = {
+        0: 'mpegts',
+        1: 'hls',
+        2: 'ia_hls',
+        3: 'ia_mpd',
+    }
 
     def get_live_stream_type(self):
-        stream_type_map = {0: 'mpegts',
-                           1: 'hls',
-                           2: 'ia_hls',
-                           3: 'ia_mpd'}
-
         if self.use_mpd():
             stream_type = self.get_int(constants.setting.LIVE_STREAMS + '.1', 0)
         else:
             stream_type = self.get_int(constants.setting.LIVE_STREAMS + '.2', 0)
-        return stream_type_map.get(stream_type) or stream_type_map[0]
+        return self._LIVE_STREAM_TYPES.get(stream_type) or self._LIVE_STREAM_TYPES[0]
 
     def use_adaptive_live_streams(self):
         if self.use_mpd():
@@ -220,39 +219,39 @@ class AbstractSettings(object):
         return self.get_bool(constants.setting.USE_PLAYBACK_HISTORY, False)
 
     # Selections based on max width and min height at common (utra-)wide aspect ratios
-    # 8K and 4K at 32:9, 2K at 8:3, remainder at 22:9 (2.444...)
-                                                                                          # MPD_QUALITY_SELECTION value
-    _QUALITY_SELECTIONS = ['mp4',                                                         # 8 (default)
-                           'webm',                                                        # 9
-                           {'width': 256, 'height': 105, 'label': '144p{0}{1}'},          # No setting
-                           {'width': 426, 'height': 175, 'label': '240p{0}{1}'},          # 0
-                           {'width': 640, 'height': 263, 'label': '360p{0}{1}'},          # 1
-                           {'width': 854, 'height': 350, 'label': '480p{0}{1}'},          # 2
-                           {'width': 1280, 'height': 525, 'label': '720p{0} (HD){1}'},    # 3
-                           {'width': 1920, 'height': 787, 'label': '1080p{0} (FHD){1}'},  # 4
-                           {'width': 2560, 'height': 984, 'label': '1440p{0} (2K){1}'},   # 5
-                           {'width': 3840, 'height': 1080, 'label': '2160p{0} (4K){1}'},  # 6
-                           {'width': 7680, 'height': 3148, 'label': '4320p{0} (8K){1}'},  # 7
-                           {'width': 0, 'height': 0, 'label': '{2}p{0}{1}'}]              # Unknown quality
+    _QUALITY_SELECTIONS = {                                                 # Setting | Resolution
+        7:   {'width': 7680, 'height': 3148, 'label': '4320p{0} (8K){1}'},  #   7     |   4320p 8K
+        6:   {'width': 3840, 'height': 1080, 'label': '2160p{0} (4K){1}'},  #   6     |   2160p 4K
+        5:   {'width': 2560, 'height': 984, 'label': '1440p{0} (QHD){1}'},  #   5     |   1440p 2.5K / QHD
+        4.1: {'width': 2048, 'height': 858, 'label': '1152p{0} (2K){1}'},   #   N/A   |   1152p 2K / QWXGA
+        4:   {'width': 1920, 'height': 787, 'label': '1080p{0} (FHD){1}'},  #   4     |   1080p FHD
+        3:   {'width': 1280, 'height': 525, 'label': '720p{0} (HD){1}'},    #   3     |   720p  HD
+        2:   {'width': 854, 'height': 350, 'label': '480p{0}{1}'},          #   2     |   480p
+        1:   {'width': 640, 'height': 263, 'label': '360p{0}{1}'},          #   1     |   360p
+        0:   {'width': 426, 'height': 175, 'label': '240p{0}{1}'},          #   0     |   240p
+        -1:  {'width': 256, 'height': 105, 'label': '144p{0}{1}'},          #   N/A   |   144p
+        -2:  {'width': 0, 'height': 0, 'label': '{2}p{0}{1}'},              #   N/A   |   Custom
+    }
 
-    def get_mpd_video_qualities(self, list_all=False):
+    def get_mpd_video_qualities(self):
         if not self.use_mpd_videos():
             return []
-        if list_all:
-            # to be converted to selection index 2
-            selected = 7
-        else:
-            selected = self.get_int(constants.setting.MPD_QUALITY_SELECTION, 8)
-        if 8 <= selected <= 9:
-            # converted to selection index 0 or 1
-            return self._QUALITY_SELECTIONS[selected - 8]
-        # converted to selection index starting from 2
-        qualities = self._QUALITY_SELECTIONS[2:]
-        del qualities[2 + selected:-1]
-        return qualities
+        selected = self.get_int(constants.setting.MPD_QUALITY_SELECTION, 4)
+        return [quality for key, quality in self._QUALITY_SELECTIONS.items()
+                if selected >= key]
 
-    def mpd_30fps_limit(self):
-        return self.get_bool(constants.setting.MPD_30FPS_LIMIT, False)
+    def stream_features(self):
+        return self.get_string(constants.setting.MPD_STREAM_FEATURES, '').split(',')
+
+    _STREAM_SELECT = {
+        1: 'auto',
+        2: 'list',
+        3: 'auto+list',
+    }
+
+    def stream_select(self):
+        select_type = self.get_int(constants.setting.MPD_STREAM_SELECT, 1)
+        return self._STREAM_SELECT.get(select_type) or self._STREAM_SELECT[1]
 
     def remote_friendly_search(self):
         return self.get_bool(constants.setting.REMOTE_FRIENDLY_SEARCH, False)
