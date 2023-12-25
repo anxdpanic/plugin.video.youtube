anxdpanic/plugin.video.youtube

commit 72480b5b5c606dd3f4540d94312fa9e996a31e9d

Support Youtube Clips
- Closes #450
- Support following plugin url formats:
  - plugin://plugin.video.youtube/uri2addon/?uri=https://www.youtube.com/clip/<clip_id>
  - plugin://plugin.video.youtube/play/?video_id=<video_id>&start=<start_time>&end=<end_time>&clip=true
- optional clip parameter will loop the clip between start_time and end_time, otherwise video will stop after end_time is reached

resources/lib/youtube_plugin/youtube/helper/url_to_item_converter.py
@@ -13,11 +13,11 @@ from urllib.parse import parse_qsl, urlparse
 
 from . import utils
 from ...kodion.items import DirectoryItem, UriItem, VideoItem
+from ...kodion.utils import duration_to_seconds
 
 
 class UrlToItemConverter(object):
     RE_PATH_ID = re.compile(r'/\w+/(?P<id>[^/?#]+)', re.I)
-    RE_SEEK_TIME = re.compile(r'\d+')
     VALID_HOSTNAMES = {
         'youtube.com',
         'www.youtube.com',
@@ -47,73 +47,67 @@ class UrlToItemConverter(object):
             return
 
         url_params = dict(parse_qsl(parsed_url.query))
-        path = parsed_url.path.lower()
+        new_params = {
+            new: process(url_params[old]) if process else url_params[old]
+            for old, new, process in (
+                ('end', 'end', duration_to_seconds),
+                ('start', 'start', duration_to_seconds),
+                ('t', 'seek', duration_to_seconds),
+                ('list', 'playlist_id', False),
+                ('v', 'video_id', False),
+                ('live', 'live', False),
+                ('clip', 'clip', False),
+            )
+            if old in url_params
+        }
 
-        channel_id = live = playlist_id = seek_time = video_id = None
-        if path == '/watch':
-            video_id = url_params.get('v')
-            playlist_id = url_params.get('list')
-            seek_time = url_params.get('t')
-        elif path == '/playlist':
-            playlist_id = url_params.get('list')
+        path = parsed_url.path.rstrip('/').lower()
+        channel_id = video_id = None
+        if path.startswith(('/playlist', '/watch')):
+            pass
         elif path.startswith('/channel/'):
             re_match = self.RE_PATH_ID.match(parsed_url.path)
             channel_id = re_match.group('id')
-            if '/live' in path:
-                live = 1
-        elif path.startswith(('/live/', '/shorts/')):
+            if path.endswith(('/live', '/streams')):
+                new_params['live'] = 1
+        elif path.startswith(('/clip/', '/embed/', '/live/', '/shorts/')):
             re_match = self.RE_PATH_ID.match(parsed_url.path)
-            video_id = re_match.group('id')
-            seek_time = url_params.get('t')
+            new_params['video_id'] = re_match.group('id')
         else:
             context.log_debug('Unknown path "{0}" in url "{1}"'.format(
                 parsed_url.path, url
             ))
             return
 
-        if video_id:
-            plugin_params = {
-                'video_id': video_id,
-            }
-            if seek_time:
-                seek_time = sum(
-                    int(number) * seconds_per_unit
-                    for number, seconds_per_unit in zip(
-                        reversed(re.findall(self.RE_SEEK_TIME, seek_time)),
-                        (1, 60, 3600, 86400)
-                    )
-                    if number
-                )
-                plugin_params['seek'] = seek_time
-            if playlist_id:
-                plugin_params['playlist_id'] = playlist_id
+        if 'video_id' in new_params:
+            video_id = new_params['video_id']
             video_item = VideoItem(
-                '', context.create_uri(['play'], plugin_params)
+                '', context.create_uri(['play'], new_params)
             )
             self._video_id_dict[video_id] = video_item
 
-        elif playlist_id:
+        elif 'playlist_id' in new_params:
+            playlist_id = new_params['playlist_id']
             if self._flatten:
                 self._playlist_ids.append(playlist_id)
-            else:
-                playlist_item = DirectoryItem(
-                    '', context.create_uri(['playlist', playlist_id])
-                )
-                playlist_item.set_fanart(provider.get_fanart(context))
-                self._playlist_id_dict[playlist_id] = playlist_item
+                return
+
+            playlist_item = DirectoryItem(
+                '', context.create_uri(['playlist', playlist_id]), new_params
+            )
+            playlist_item.set_fanart(provider.get_fanart(context))
+            self._playlist_id_dict[playlist_id] = playlist_item
 
         elif channel_id:
             if self._flatten:
-                if live:
-                    context.set_param('live', live)
                 self._channel_ids.append(channel_id)
-            else:
-                channel_item = DirectoryItem(
-                    '', context.create_uri(['channel', channel_id],
-                                           {'live': live} if live else None)
-                )
-                channel_item.set_fanart(provider.get_fanart(context))
-                self._channel_id_dict[channel_id] = channel_item
+                return
+
+            channel_item = DirectoryItem(
+                '', context.create_uri(['channel', channel_id], new_params)
+            )
+            channel_item.set_fanart(provider.get_fanart(context))
+            self._channel_id_dict[channel_id] = channel_item
 
         else:
             context.log_debug('No items found in url "{0}"'.format(url))

resources/lib/youtube_plugin/kodion/context/abstract_context.py
@@ -32,6 +32,7 @@ class AbstractContext(object):
         'ask_for_quality',
         'audio_only',
         'confirmed',
+        'clip',
         'enable',
         'hide_folders',
         'hide_live',

resources/lib/youtube_plugin/kodion/utils/methods.py
@@ -272,8 +272,9 @@ def friendly_number(number, precision=3, scale=('', 'K', 'M', 'B')):
     ).rstrip('0').rstrip('.') + scale[magnitude], _input
 
 
-_RE_PERIODS = re.compile(r'(\d+)(d|h|m|s)')
+_RE_PERIODS = re.compile(r'([\d.]+)(d|h|m|s|$)')
 _SECONDS_IN_PERIODS = {
+    '': 1,       # 1 second for unitless period
     's': 1,      # 1 second
     'm': 60,     # 1 minute
     'h': 3600,   # 1 hour
@@ -285,11 +286,12 @@ def duration_to_seconds(duration):
     if ':' in duration:
         seconds = 0
         for part in duration.split(':'):
-            seconds = seconds * 60 + int(part, 10)
+            seconds = seconds * 60 + (float(part) if '.' in part else int(part))
         return seconds
     return sum(
-        int(number) * _SECONDS_IN_PERIODS[period]
-        for number, period in re.findall(_RE_PERIODS, duration)
+        (float(number) if '.' in number else int(number))
+        * _SECONDS_IN_PERIODS.get(period, 1)
+        for number, period in re.findall(_RE_PERIODS, duration.lower())
     )
 
 

resources/lib/youtube_plugin/kodion/utils/player.py
@@ -56,6 +56,7 @@ class PlaybackMonitorThread(threading.Thread):
         use_local_history = self.playback_json.get('use_local_history', False)
         playback_stats = self.playback_json.get('playback_stats')
         refresh_only = self.playback_json.get('refresh_only', False)
+        clip = self.playback_json.get('clip', False)
 
         player = self.player
 
@@ -166,7 +167,7 @@ class PlaybackMonitorThread(threading.Thread):
                         pass
 
             if player._end_time and self.current_time >= player._end_time:
-                if player._start_time:
+                if clip and player._start_time:
                     player.seekTime(player._start_time)
                 else:
                     player.stop()
@@ -403,7 +404,10 @@ class YouTubePlayer(xbmc.Player):
                                ', '.join([thread.video_id for thread in active_threads]))
         self.threads = active_threads
 
-    def onPlayBackStarted(self):
+    def onAVStarted(self):
+        if not self.ui.busy_dialog_active():
+            self.ui.clear_property('busy')
+
         playback_json = self.ui.get_property('playback_json')
         if not playback_json:
             return
@@ -426,6 +430,9 @@ class YouTubePlayer(xbmc.Player):
                                                   playback_json))
 
     def onPlayBackEnded(self):
+        if not self.ui.busy_dialog_active():
+            self.ui.clear_property('busy')
+
         self.stop_threads()
         self.cleanup_threads()
 

resources/lib/youtube_plugin/youtube/helper/url_resolver.py
@@ -9,6 +9,7 @@
 """
 
 import re
+from html import unescape
 from urllib.parse import parse_qsl, urlencode, urlparse
 
 from ...kodion.network import BaseRequestsClass
@@ -51,6 +52,15 @@ class AbstractResolver(BaseRequestsClass):
 
 
 class YouTubeResolver(AbstractResolver):
+    _RE_CHANNEL_URL = re.compile(r'<meta property="og:url" content="'
+                                 r'(?P<channel_url>[^"]+)'
+                                 r'">')
+    _RE_CLIP_DETAILS = re.compile(r'(<meta property="og:video:url" content="'
+                                  r'(?P<video_url>[^"]+)'
+                                  r'">)'
+                                  r'|("startTimeMs":"(?P<start_time>\d+)")'
+                                  r'|("endTimeMs":"(?P<end_time>\d+)")')
+
     def __init__(self, *args, **kwargs):
         super(YouTubeResolver, self).__init__(*args, **kwargs)
 
@@ -67,6 +77,7 @@ class YouTubeResolver(AbstractResolver):
                 '/@',
                 '/c/',
                 '/channel/',
+                '/clip',
                 '/user/',
         )):
             return 'GET'
@@ -118,13 +129,47 @@ class YouTubeResolver(AbstractResolver):
         if response.status_code != 200:
             return url
 
+        if path.startswith('/clip'):
+            all_matches = self._RE_CLIP_DETAILS.finditer(response.text)
+            num_matched = 0
+            url_components = params = start_time = end_time = None
+            for matches in all_matches:
+                matches = matches.groupdict()
+
+                if not num_matched & 1:
+                    url = matches['video_url']
+                    if url:
+                        num_matched += 1
+                        url_components = urlparse(unescape(url))
+                        params = dict(parse_qsl(url_components.query))
+
+                if not num_matched & 2:
+                    start_time = matches['start_time']
+                    if start_time:
+                        start_time = int(start_time) / 1000
+                        num_matched += 2
+
+                if not num_matched & 4:
+                    end_time = matches['end_time']
+                    if end_time:
+                        end_time = int(end_time) / 1000
+                        num_matched += 4
+
+                if num_matched != 7:
+                    continue
+
+                params.update({
+                    'clip': True,
+                    'start': start_time,
+                    'end': end_time,
+                })
+                return url_components._replace(query=urlencode(params)).geturl()
+
         # we try to extract the channel id from the html content
         # With the channel id we can construct a URL we already work with
         # https://www.youtube.com/channel/<CHANNEL_ID>
-        if method == 'GET':
-            match = re.search(
-                r'<meta property="og:url" content="(?P<channel_url>.+?)">',
-                response.text)
+        elif method == 'GET':
+            match = self._RE_CHANNEL_URL.search(response.text)
             if match:
                 return match.group('channel_url')
 

resources/lib/youtube_plugin/youtube/helper/yt_play.py
@@ -99,8 +99,10 @@ def play_video(provider, context):
 
     if start_time:
         video_item.set_start_time(start_time)
-    if end_time:
-        video_item.set_duration_from_seconds(end_time)
+    # Setting the duration based on end_time can cause issues with
+    # listing/sorting and other addons that monitor playback
+    # if end_time:
+    #     video_item.set_duration_from_seconds(end_time)
 
     play_count = use_play_data and video_item.get_play_count() or 0
     playback_stats = video_stream.get('playback_stats')
@@ -117,6 +119,7 @@ def play_video(provider, context):
         'seek_time': seek_time,
         'start_time': start_time,
         'end_time': end_time,
+        'clip': params.get('clip'),
         'refresh_only': screensaver
     }
 

commit 58195b765658baa4ca09561edf4541212a0631ff

Fix regression after 72480b5 caused by typo

resources/lib/youtube_plugin/youtube/helper/url_to_item_converter.py
@@ -97,7 +97,7 @@ class UrlToItemConverter(object):
                 return
 
             playlist_item = DirectoryItem(
-                '', context.create_uri(['playlist', playlist_id]), new_params
+                '', context.create_uri(['playlist', playlist_id], new_params),
             )
             playlist_item.set_fanart(provider.get_fanart(context))
             self._playlist_id_dict[playlist_id] = playlist_item