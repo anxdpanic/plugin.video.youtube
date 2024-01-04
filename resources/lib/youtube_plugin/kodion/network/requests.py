anxdpanic/plugin.video.youtube

commit 978312d69c6fcfa2632e939e2dd87ee85fac42d2

Re-org to prevent issues with circular imports
- Move http_server, ip_api, and requests to kodion.network module from
  kodion.utils
- Split kodion.impl to
  - kodion.context
  - kodion.player
  - kodion.plugin
  - kodion.settings
  - kodion.ui
- Remove context parameter from BaseRequestsClass, requests now imports
  Settings directly and can be imported by other modules in kodion
- Update BaseRequestsClass.request to use same params as parent class

resources/lib/youtube_plugin/kodion/network/requests.py
@@ -15,6 +15,14 @@ from requests import Session
 from requests.adapters import HTTPAdapter, Retry
 from requests.exceptions import RequestException
 
+from ..logger import log_error
+from ..settings import Settings
+
+from xbmcaddon import Addon
+
+
+_settings = Settings(Addon(id='plugin.video.youtube'))
+
 
 class BaseRequestsClass(object):
     http_adapter = HTTPAdapter(
@@ -28,10 +36,9 @@ class BaseRequestsClass(object):
         )
     )
 
-    def __init__(self, context, exc_type=RequestException):
-        self._context = context
-        self._verify = self._context.get_settings().verify_ssl()
-        self._timeout = self._context.get_settings().get_timeout()
+    def __init__(self, exc_type=RequestException):
+        self._verify = _settings.verify_ssl()
+        self._timeout = _settings.get_timeout()
         self._default_exc = exc_type
 
         self._session = Session()
@@ -46,20 +53,38 @@ class BaseRequestsClass(object):
         self._session.close()
 
     def request(self, url, method='GET',
-                cookies=None, data=None, headers=None, json=None, params=None,
+                params=None, data=None, headers=None, cookies=None, files=None,
+                auth=None, timeout=None, allow_redirects=None, proxies=None,
+                hooks=None, stream=None, verify=None, cert=None, json=None,
+                # Custom event hook implementation
+                # See _login_json_hook and _login_error_hook in login_client.py
+                # for example usage
                 response_hook=None, error_hook=None,
                 error_title=None, error_info=None, raise_exc=False, **_):
+        if timeout is None:
+            timeout = self._timeout
+        if verify is None:
+            verify = self._verify
+        if allow_redirects is None:
+            allow_redirects = True
+
         response = None
         try:
             response = self._session.request(method, url,
-                                             verify=self._verify,
-                                             allow_redirects=True,
-                                             timeout=self._timeout,
-                                             cookies=cookies,
+                                             params=params,
                                              data=data,
                                              headers=headers,
-                                             json=json,
-                                             params=params)
+                                             cookies=cookies,
+                                             files=files,
+                                             auth=auth,
+                                             timeout=timeout,
+                                             allow_redirects=allow_redirects,
+                                             proxies=proxies,
+                                             hooks=hooks,
+                                             stream=stream,
+                                             verify=verify,
+                                             cert=cert,
+                                             json=json,)
             if response_hook:
                 response = response_hook(response)
             else:
@@ -106,15 +131,15 @@ class BaseRequestsClass(object):
                     )
                 )
 
-            self._context.log_error('\n'.join([part for part in [
+            log_error('\n'.join([part for part in [
                 error_title, error_info, response_text, stack_trace, exc_tb
             ] if part]))
 
             if raise_exc:
                 if isinstance(raise_exc, BaseException):
-                    raise raise_exc from exc
-                if not callable(raise_exc):
-                    raise self._default_exc(error_title) from exc
-                raise raise_exc(error_title) from exc
+                    raise raise_exc(exc)
+                if callable(raise_exc):
+                    raise raise_exc(error_title)(exc)
+                raise self._default_exc(error_title)(exc)
 
         return response

resources/lib/youtube_plugin/youtube/client/request_client.py
@@ -7,7 +7,7 @@
     See LICENSES/GPL-2.0-only for more information.
 """
 
-from ...kodion.utils.requests import BaseRequestsClass
+from ...kodion.network import BaseRequestsClass
 from ...youtube.youtube_exceptions import YouTubeException
 
 
@@ -255,10 +255,8 @@ class YouTubeRequestClient(BaseRequestsClass):
         },
     }
 
-    def __init__(self, context):
-        super(YouTubeRequestClient, self).__init__(
-            context=context, exc_type=YouTubeException
-        )
+    def __init__(self):
+        super(YouTubeRequestClient, self).__init__(exc_type=YouTubeException)
 
     @staticmethod
     def json_traverse(json_data, path):

resources/lib/youtube_authentication.py
@@ -8,7 +8,7 @@
 """
 
 from youtube_plugin.youtube.provider import Provider
-from youtube_plugin.kodion.impl import Context
+from youtube_plugin.kodion.context import Context
 from youtube_plugin.youtube.helper import yt_login
 
 # noinspection PyUnresolvedReferences

resources/lib/youtube_plugin/kodion/__init__.py
@@ -18,7 +18,7 @@ from .register_provider_path import RegisterProviderPath
 from .abstract_provider import AbstractProvider
 
 # import specialized implementation into the kodion namespace
-from .impl import Context
+from .context import Context
 
 from . import logger
 

resources/lib/youtube_plugin/kodion/context/__init__.py
@@ -1,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Copyright (C) 2023-present plugin.video.youtube
+
+    SPDX-License-Identifier: GPL-2.0-only
+    See LICENSES/GPL-2.0-only for more information.
+"""
+
+from .xbmc.xbmc_context import XbmcContext as Context
+
+
+__all__ = ('Context', )

resources/lib/youtube_plugin/kodion/context/xbmc/xbmc_context.py
@@ -21,11 +21,12 @@ import xbmcplugin
 import xbmcvfs
 
 from ..abstract_context import AbstractContext
-from .xbmc_plugin_settings import XbmcPluginSettings
-from .xbmc_context_ui import XbmcContextUI
-from .xbmc_playlist import XbmcPlaylist
-from .xbmc_player import XbmcPlayer
+from ...player.xbmc.xbmc_playlist import XbmcPlaylist
+from ...player.xbmc.xbmc_player import XbmcPlayer
+from ...settings.xbmc.xbmc_plugin_settings import XbmcPluginSettings
+from ...ui.xbmc.xbmc_context_ui import XbmcContextUI
 from ... import utils
+
 
 try:
     xbmc.translatePath = xbmcvfs.translatePath

resources/lib/youtube_plugin/kodion/impl/__init__.py
@@ -1,17 +1,0 @@
-# -*- coding: utf-8 -*-
-"""
-
-    Copyright (C) 2014-2016 bromix (plugin.video.youtube)
-    Copyright (C) 2016-2018 plugin.video.youtube
-
-    SPDX-License-Identifier: GPL-2.0-only
-    See LICENSES/GPL-2.0-only for more information.
-"""
-
-from .xbmc.xbmc_plugin_settings import XbmcPluginSettings as Settings
-from .xbmc.xbmc_context import XbmcContext as Context
-from .xbmc.xbmc_context_ui import XbmcContextUI as ContextUI
-from .xbmc.xbmc_runner import XbmcRunner as Runner
-
-
-__all__ = ['Settings', 'Context', 'ContextUI', 'Runner']

commit 10896ace41855d892f64aa10e4a3d63443c48b86

Follow up to d713443
- Fix up bug introduced in 978312d while trying to maintain Python 2 compatibility

resources/lib/youtube_plugin/kodion/network/requests.py
@@ -54,7 +54,7 @@ class BaseRequestsClass(object):
         elif exc_type:
             self._default_exc = (RequestException, exc_type)
         else:
-            self._default_exc = RequestException
+            self._default_exc = (RequestException,)
 
     def __del__(self):
         self._session.close()
@@ -101,6 +101,9 @@ class BaseRequestsClass(object):
                                              verify=verify,
                                              cert=cert,
                                              json=json)
+            if not response:
+                raise self._default_exc[0]
+
             if response_hook:
                 if response_hook_kwargs is None:
                     response_hook_kwargs = {}
@@ -118,7 +121,7 @@ class BaseRequestsClass(object):
                 if error_hook_kwargs is None:
                     error_hook_kwargs = {}
                 error_hook_kwargs['exc'] = exc
-                error_hook_kwargs['response'] = response
+                error_hook_kwargs['response'] = response or exc.response
                 error_response = error_hook(**error_hook_kwargs)
                 _title, _info, _detail, _response, _trace, _exc = error_response
                 if _title is not None:
@@ -161,10 +164,13 @@ class BaseRequestsClass(object):
             ] if part]))
 
             if raise_exc:
+                if not callable(raise_exc):
+                    raise_exc = self._default_exc[-1]
+                raise_exc = raise_exc(error_title)
+
                 if isinstance(raise_exc, BaseException):
-                    raise raise_exc(exc)
-                if callable(raise_exc):
-                    raise raise_exc(error_title)(exc)
-                raise self._default_exc(error_title)(exc)
+                    raise_exc.__cause__ = exc
+                    raise raise_exc
+                raise exc
 
         return response

resources/lib/youtube_plugin/youtube/client/request_client.py
@@ -264,7 +264,7 @@ class YouTubeRequestClient(BaseRequestsClass):
         elif exc_type:
             exc_type = (YouTubeException, exc_type)
         else:
-            exc_type = YouTubeException
+            exc_type = (YouTubeException,)
         super(YouTubeRequestClient, self).__init__(exc_type=exc_type)
 
     @classmethod