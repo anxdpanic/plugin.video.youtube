0795387d67888f00e2ae8dc24e6c734a3e6e6dd6
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-01-08 14:38:28 +1100

    Move infotagger import to compatibility module

--- resources/lib/youtube_plugin/kodion/compatibility/__init__.py
+++ resources/lib/youtube_plugin/kodion/compatibility/__init__.py
@@ -26,6 +26,8 @@ try:
     import xbmcplugin
     import xbmcvfs
 
+    from infotagger.listitem import set_info_tag
+
 except ImportError:
     import BaseHTTPServer
     from contextlib import contextmanager as _contextmanager
@@ -91,12 +93,36 @@ except ImportError:
     xbmcvfs.File = _file_closer
     xbmcvfs.translatePath = xbmc.translatePath
 
+    def set_info_tag(listitem, infolabels, tag_type, *_args, **_kwargs):
+        listitem.setInfo(tag_type, infolabels)
+        return ListItemInfoTag(listitem, tag_type)
+
+
+    class ListItemInfoTag(object):
+        __slots__ = ('__li__',)
+
+        def __init__(self, listitem, *_args, **_kwargs):
+            self.__li__ = listitem
+
+        def add_stream_info(self, *args, **kwargs):
+            return self.__li__.addStreamInfo(*args, **kwargs)
+
+        def set_resume_point(self,
+                             infoproperties,
+                             resume_key='ResumeTime',
+                             total_key='TotalTime'):
+            if resume_key in infoproperties:
+                infoproperties[resume_key] = str(infoproperties[resume_key])
+            if total_key in infoproperties:
+                infoproperties[total_key] = str(infoproperties[total_key])
+
 
 __all__ = (
     'BaseHTTPServer',
     'parse_qs',
     'parse_qsl',
     'quote',
+    'set_info_tag',
     'unescape',
     'unquote',
     'urlencode',
--- resources/lib/youtube_plugin/kodion/ui/xbmc/xbmc_items.py
+++ resources/lib/youtube_plugin/kodion/ui/xbmc/xbmc_items.py
@@ -11,35 +11,9 @@
 from __future__ import absolute_import, division, unicode_literals
 
 from . import info_labels
-from ...compatibility import xbmcgui
+from ...compatibility import set_info_tag, xbmcgui
 from ...items import AudioItem, UriItem, VideoItem
 from ...utils import current_system_version, datetime_parser
-
-try:
-    from infotagger.listitem import set_info_tag
-except ImportError:
-    def set_info_tag(listitem, infolabels, tag_type, *_args, **_kwargs):
-        listitem.setInfo(tag_type, infolabels)
-        return ListItemInfoTag(listitem, tag_type)
-
-
-    class ListItemInfoTag(object):
-        __slots__ = ('__li__',)
-
-        def __init__(self, listitem, *_args, **_kwargs):
-            self.__li__ = listitem
-
-        def add_stream_info(self, *args, **kwargs):
-            return self.__li__.addStreamInfo(*args, **kwargs)
-
-        def set_resume_point(self,
-                             infoproperties,
-                             resume_key='ResumeTime',
-                             total_key='TotalTime'):
-            if resume_key in infoproperties:
-                infoproperties[resume_key] = str(infoproperties[resume_key])
-            if total_key in infoproperties:
-                infoproperties[total_key] = str(infoproperties[total_key])
 
 
 def video_playback_item(context, video_item, show_fanart=None):
