218d2f0bc7e96cbfa76319280411fa80d3da05bd
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-03-13 09:29:11 +1100

    Add ViewManager
    - Updated to be more self-contained and work better with unsupported skins
    - TODO: Add support for setting default sort order and sort direction
    
    Fix content type not being set to episodes
    
    - Fix #586, #589
    
    Update for restructure of xbmc_plugin
    
    - Only set view mode if directory items successfully added

--- resources/lib/youtube_plugin/kodion/ui/xbmc/xbmc_context_ui.py
+++ resources/lib/youtube_plugin/kodion/ui/xbmc/xbmc_context_ui.py
@@ -10,6 +10,7 @@
 
 from __future__ import absolute_import, division, unicode_literals
 
+from .view_manager import ViewManager
 from .xbmc_progress_dialog import XbmcProgressDialog, XbmcProgressDialogBG
 from ..abstract_context_ui import AbstractContextUI
 from ...compatibility import xbmc, xbmcgui
@@ -21,12 +22,19 @@ class XbmcContextUI(AbstractContextUI):
     def __init__(self, context):
         super(XbmcContextUI, self).__init__()
         self._context = context
+        self._view_manager = None
 
     def create_progress_dialog(self, heading, text=None, background=False):
         if background:
             return XbmcProgressDialogBG(heading, text)
 
         return XbmcProgressDialog(heading, text)
+
+    def get_view_manager(self):
+        if self._view_manager is None:
+            self._view_manager = ViewManager(self._context)
+
+        return self._view_manager
 
     def on_keyboard_input(self, title, default='', hidden=False):
         # Starting with Gotham (13.X > ...)
