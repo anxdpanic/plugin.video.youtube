5782e7b2577abef15102376a0a4e008dbb9b1e19
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-04-03 03:33:07 +1100

    Tidy up some exception catching

--- resources/lib/youtube_plugin/kodion/utils/methods.py
+++ resources/lib/youtube_plugin/kodion/utils/methods.py
@@ -323,10 +323,16 @@ def merge_dicts(item1, item2, templates=None, _=Ellipsis):
     return new or _
 
 
-def get_kodi_setting_value(setting):
+def get_kodi_setting_value(setting, process=None):
     response = jsonrpc(method='Settings.GetSettingValue',
                        params={'setting': setting})
-    return response.get('result', {}).get('value')
+    try:
+        value = response['result']['value']
+        if process:
+            return process(value)
+    except (KeyError, TypeError, ValueError):
+        return None
+    return value
 
 
 def get_kodi_setting_bool(setting):
