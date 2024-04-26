anxdpanic/plugin.video.youtube

commit 2b58a0136da9fcc45a508dd86b1e7a18c4a773cf

Fix Python 2 JSONEncoder internally trying to join unicode values with str values

resources/lib/youtube_plugin/kodion/items/base_item.py
@@ -239,4 +239,7 @@ class _Encoder(json.JSONEncoder):
                 }
         else:
             output = obj
-        return output if nested else super(_Encoder, self).encode(output)
+
+        if nested:
+            return to_str(output)
+        return super(_Encoder, self).encode(output)

commit d255e219f28b4e77f8b4a0d2e04de83a682ce446

Fix adding and viewing items to internal bookmarks and watchlater list #720
- New bug introduced in 2b58a01
- Properly fix Python 2 JSONEncoder issues

resources/lib/youtube_plugin/kodion/items/base_item.py
@@ -210,17 +210,10 @@ class BaseItem(object):
 
 class _Encoder(json.JSONEncoder):
     def encode(self, obj, nested=False):
-        if isinstance(obj, string_type):
-            output = to_str(obj)
-        elif isinstance(obj, dict):
-            output = {to_str(key): self.encode(value, nested=True)
-                      for key, value in obj.items()}
-        elif isinstance(obj, (list, tuple)):
-            output = [self.encode(item, nested=True) for item in obj]
-        elif isinstance(obj, (date, datetime)):
+        if isinstance(obj, (date, datetime)):
             class_name = obj.__class__.__name__
             if 'fromisoformat' in dir(obj):
-                output = {
+                obj = {
                     '__class__': class_name,
                     '__isoformat__': obj.isoformat(),
                 }
@@ -232,14 +225,22 @@ class _Encoder(json.JSONEncoder):
                         format_string = '%Y-%m-%dT%H:%M:%S'
                 else:
                     format_string = '%Y-%m-%d'
-                output = {
+                obj = {
                     '__class__': class_name,
                     '__format_string__': format_string,
                     '__value__': obj.strftime(format_string)
                 }
+
+        if isinstance(obj, string_type):
+            output = to_str(obj)
+        elif isinstance(obj, dict):
+            output = {to_str(key): self.encode(value, nested=True)
+                      for key, value in obj.items()}
+        elif isinstance(obj, (list, tuple)):
+            output = [self.encode(item, nested=True) for item in obj]
         else:
             output = obj
 
         if nested:
-            return to_str(output)
+            return output
         return super(_Encoder, self).encode(output)