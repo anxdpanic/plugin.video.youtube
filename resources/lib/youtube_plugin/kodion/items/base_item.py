39c3a9c083ea444254a99e581b7525b59b1038d9
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-05-13 00:16:26 +1000

    Allow checking whether non-default fanart has been set for an item

--- resources/lib/youtube_plugin/kodion/items/base_item.py
+++ resources/lib/youtube_plugin/kodion/items/base_item.py
@@ -108,7 +108,6 @@ class BaseItem(object):
 
     def set_fanart(self, fanart):
         if not fanart:
-            self._fanart = '{0}/fanart.jpg'.format(MEDIA_PATH)
             return
 
         if '{media}/' in fanart:
@@ -116,8 +115,10 @@ class BaseItem(object):
         else:
             self._fanart = fanart
 
-    def get_fanart(self):
-        return self._fanart
+    def get_fanart(self, default=True):
+        if self._fanart or not default:
+            return self._fanart
+        return '{0}/fanart.jpg'.format(MEDIA_PATH)
 
     def add_context_menu(self, context_menu, position='end', replace=False):
         context_menu = (item for item in context_menu if item)
