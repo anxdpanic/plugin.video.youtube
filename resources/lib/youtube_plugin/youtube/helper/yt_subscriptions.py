797ebf66833e16212357d726ef9209a29533f839
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-05-27 08:57:19 +1000

    Avoid unnecessary duplicated client creation/checks in yt_subscriptions

--- resources/lib/youtube_plugin/youtube/helper/yt_subscriptions.py
+++ resources/lib/youtube_plugin/youtube/helper/yt_subscriptions.py
@@ -15,8 +15,8 @@ from ...kodion import KodionException
 from ...kodion.items import UriItem
 
 
-def _process_list(provider, context):
-    json_data = provider.get_client(context).get_subscription(
+def _process_list(provider, context, client):
+    json_data = client.get_subscription(
         'mine', page_token=context.get_param('page_token', '')
     )
     if not json_data:
@@ -24,7 +24,7 @@ def _process_list(provider, context):
     return v3.response_to_items(provider, context, json_data)
 
 
-def _process_add(provider, context):
+def _process_add(_provider, context, client):
     listitem_subscription_id = context.get_listitem_detail('subscription_id')
 
     subscription_id = context.get_param('subscription_id', '')
@@ -32,23 +32,22 @@ def _process_add(provider, context):
             and listitem_subscription_id.lower().startswith('uc')):
         subscription_id = listitem_subscription_id
 
-    if subscription_id:
-        json_data = provider.get_client(context).subscribe(subscription_id)
-        if not json_data:
-            return False
+    if not subscription_id:
+        return False
 
-        context.get_ui().show_notification(
-            context.localize('subscribed.to.channel'),
-            time_ms=2500,
-            audible=False
-        )
+    json_data = client.subscribe(subscription_id)
+    if not json_data:
+        return False
 
-        return True
+    context.get_ui().show_notification(
+        context.localize('subscribed.to.channel'),
+        time_ms=2500,
+        audible=False
+    )
+    return True
 
-    return False
 
-
-def _process_remove(provider, context):
+def _process_remove(_provider, context, client):
     listitem_subscription_id = context.get_listitem_detail('channel_subscription_id')
     listitem_channel_id = context.get_listitem_detail('channel_id')
 
@@ -61,9 +60,9 @@ def _process_remove(provider, context):
         channel_id = listitem_channel_id
 
     if subscription_id:
-        success = provider.get_client(context).unsubscribe(subscription_id)
+        success = client.unsubscribe(subscription_id)
     elif channel_id:
-        success = provider.get_client(context).unsubscribe_channel(channel_id)
+        success = client.unsubscribe_channel(channel_id)
     else:
         success = False
 
@@ -81,14 +80,14 @@ def _process_remove(provider, context):
 
 def process(method, provider, context):
     # we need a login
-    _ = provider.get_client(context)
+    client = provider.get_client(context)
     if not provider.is_logged_in():
         return UriItem(context.create_uri(('sign', 'in')))
 
     if method == 'list':
-        return _process_list(provider, context)
+        return _process_list(provider, context, client)
     if method == 'add':
-        return _process_add(provider, context)
+        return _process_add(provider, context, client)
     if method == 'remove':
-        return _process_remove(provider, context)
+        return _process_remove(provider, context, client)
     raise KodionException("Unknown subscriptions method '%s'" % method)
