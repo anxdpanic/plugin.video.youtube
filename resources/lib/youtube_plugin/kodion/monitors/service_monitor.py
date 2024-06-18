3726193d1828a7bc2c934046a2bf00751724c2e7
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-06-19 06:47:57 +1000

    Simplify and tidy up ServiceMonitor

--- resources/lib/youtube_plugin/kodion/monitors/service_monitor.py
+++ resources/lib/youtube_plugin/kodion/monitors/service_monitor.py
@@ -32,12 +32,10 @@ class ServiceMonitor(xbmc.Monitor):
     def __init__(self, context):
         self._context = context
         settings = context.get_settings()
-        self._use_httpd = (settings.use_isa()
-                           or settings.api_config_page()
-                           or settings.support_alternative_player())
-        address, port = get_connect_address(self._context)
-        self._old_httpd_address = self._httpd_address = address
-        self._old_httpd_port = self._httpd_port = port
+
+        self._httpd_address, self._httpd_port = get_connect_address(context)
+        self._old_httpd_address = self._httpd_address
+        self._old_httpd_port = self._httpd_port
         self._whitelist = settings.httpd_whitelist()
 
         self.httpd = None
@@ -46,7 +44,8 @@ class ServiceMonitor(xbmc.Monitor):
         self.refresh = False
         self.interrupt = False
 
-        if self._use_httpd:
+        self._use_httpd = None
+        if self.httpd_required(settings):
             self.start_httpd()
 
         super(ServiceMonitor, self).__init__()
@@ -125,44 +124,34 @@ class ServiceMonitor(xbmc.Monitor):
         self._settings_changes = 0
 
         settings = self._context.get_settings(refresh=True)
-
         self.set_property(CHECK_SETTINGS)
         self.refresh_container()
 
-        use_httpd = (settings.use_isa()
-                     or settings.api_config_page()
-                     or settings.support_alternative_player())
+        httpd_started = bool(self.httpd)
+        httpd_restart = False
+
         address, port = get_connect_address(self._context)
-        whitelist = settings.httpd_whitelist()
-
-        whitelist_changed = whitelist != self._whitelist
-        port_changed = port != self._httpd_port
-        address_changed = address != self._httpd_address
-
-        if whitelist_changed:
-            self._whitelist = whitelist
-
-        if self._use_httpd != use_httpd:
-            self._use_httpd = use_httpd
-
-        if port_changed:
+        if port != self._httpd_port:
             self._old_httpd_port = self._httpd_port
             self._httpd_port = port
-
-        if address_changed:
+            httpd_restart = httpd_started
+        if address != self._httpd_address:
             self._old_httpd_address = self._httpd_address
             self._httpd_address = address
+            httpd_restart = httpd_started
 
-        if not use_httpd:
-            if self.httpd:
-                self.shutdown_httpd()
-        elif not self.httpd:
-            self.start_httpd()
-        elif port_changed or whitelist_changed or address_changed:
-            if self.httpd:
+        whitelist = settings.httpd_whitelist()
+        if whitelist != self._whitelist:
+            self._whitelist = whitelist
+            httpd_restart = httpd_started
+
+        if self.httpd_required(settings):
+            if httpd_restart:
                 self.restart_httpd()
             else:
                 self.start_httpd()
+        elif httpd_started:
+            self.shutdown_httpd()
 
     def httpd_address_sync(self):
         self._old_httpd_address = self._httpd_address
@@ -214,9 +203,14 @@ class ServiceMonitor(xbmc.Monitor):
     def ping_httpd(self):
         return self.httpd and httpd_status(self._context)
 
-    def httpd_required(self, while_sleeping=False):
+    def httpd_required(self, settings=None, while_sleeping=False):
         if while_sleeping:
             settings = self._context.get_settings()
             return (settings.api_config_page()
                     or settings.support_alternative_player())
+
+        if settings:
+            self._use_httpd = (settings.use_isa()
+                               or settings.api_config_page()
+                               or settings.support_alternative_player())
         return self._use_httpd
