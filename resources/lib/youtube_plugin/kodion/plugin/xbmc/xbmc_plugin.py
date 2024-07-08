c0141c780855876dcdfa4deccd8255858794b1b3
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-07-08 20:10:08 +1000

    Add ViewManager
    - Updated to be more self-contained and work better with unsupported skins
    - TODO: Add support for setting default sort order and sort direction
    
    Fix content type not being set to episodes
    
    - Fix #586, #589
    
    Update for restructure of xbmc_plugin
    
    - Only set view mode if directory items successfully added
    
    Fix preselect on view_manager view lists
    
    Update to match new setup wizard
    
    Update for reorganised/renamed constants fca610c
    
    Update for updated XbmcContext.apply_content 8a8247a

--- resources/lib/youtube_plugin/kodion/plugin/xbmc/xbmc_plugin.py
+++ resources/lib/youtube_plugin/kodion/plugin/xbmc/xbmc_plugin.py
@@ -264,6 +264,14 @@ class XbmcPlugin(AbstractPlugin):
             fallback = options.get(provider.FALLBACK)
             if not fallback or fallback != ui.get_property(provider.FALLBACK):
                 ui.clear_property(provider.FALLBACK)
+
+            # set alternative view mode
+            view_manager = ui.get_view_manager()
+            if view_manager.is_override_view_enabled():
+                view_mode = view_manager.get_view_mode()
+                if view_mode is not None:
+                    context.log_debug('Override view mode to "%d"' % view_mode)
+                    context.execute('Container.SetViewMode(%d)' % view_mode)
         else:
             succeeded = bool(result)
             if not succeeded:
