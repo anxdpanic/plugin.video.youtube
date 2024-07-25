acbd0654dd19f70869699e5614aac0accb3e02c0
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2024-07-25 10:11:15 +1000

    Update client details and usage

--- resources/lib/youtube_plugin/youtube/client/request_client.py
+++ resources/lib/youtube_plugin/youtube/client/request_client.py
@@ -88,14 +88,15 @@ class YouTubeRequestClient(BaseRequestsClass):
         # Limited to 720p on some videos
         'android_embedded': {
             '_id': 55,
+            '_disabled': True,
             '_query_subtitles': 'optional',
             'json': {
                 'params': _ANDROID_PARAMS,
                 'context': {
                     'client': {
                         'clientName': 'ANDROID_EMBEDDED_PLAYER',
-                        'clientVersion': '19.17.34',
                         'clientScreen': 'EMBED',
+                        'clientVersion': '19.17.34',
                         'androidSdkVersion': '34',
                         'osName': 'Android',
                         'osVersion': '14',
@@ -114,6 +115,9 @@ class YouTubeRequestClient(BaseRequestsClass):
                                ' {json[context][client][gl]}) gzip'),
                 'X-YouTube-Client-Name': '{_id}',
                 'X-YouTube-Client-Version': '{json[context][client][clientVersion]}',
+            },
+            'params': {
+                'key': _API_KEYS['android_embedded'],
             },
         },
         # 4k with HDR
@@ -207,7 +211,6 @@ class YouTubeRequestClient(BaseRequestsClass):
         },
         'media_connect_frontend': {
             '_id': 95,
-            '_access_token': KeyError,
             '_query_subtitles': True,
             'json': {
                 'context': {
