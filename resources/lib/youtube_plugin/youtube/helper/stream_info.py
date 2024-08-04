anxdpanic/plugin.video.youtube

commit cad487d3405445e587cecea872a7d21bbbc73653

Misc tidy ups
- replace more str.format calls

resources/lib/youtube_plugin/youtube/helper/stream_info.py
@@ -862,9 +862,9 @@ class StreamInfo(YouTubeRequestClient):
 
     def _get_player_page(self, client_name='web', embed=False):
         if embed:
-            url = 'https://www.youtube.com/embed/{0}'.format(self.video_id)
+            url = ''.join(('https://www.youtube.com/embed/', self.video_id))
         else:
-            url = 'https://www.youtube.com/watch?v={0}'.format(self.video_id)
+            url = ''.join(('https://www.youtube.com/watch?v=', self.video_id))
         # Manually configured cookies to avoid cookie consent redirect
         cookies = {'SOCS': 'CAISAiAD'}
 
@@ -974,16 +974,14 @@ class StreamInfo(YouTubeRequestClient):
 
     @staticmethod
     def _make_curl_headers(headers, cookies=None):
-        output = []
         if cookies:
-            output.append('Cookie={0}'.format(quote('; '.join(
-                '{0.name}={0.value}'.format(cookie)
-                for cookie in cookies
-            ))))
+            headers['Cookie'] = '; '.join([
+                '='.join((cookie.name, cookie.value)) for cookie in cookies
+            ])
         # Headers used in xbmc_items.video_playback_item'
-        output.extend('{0}={1}'.format(key, quote(value))
-                      for key, value in headers.items())
-        return '&'.join(output)
+        return '&'.join([
+            '='.join((key, quote(value))) for key, value in headers.items()
+        ])
 
     @staticmethod
     def _normalize_url(url):
@@ -1243,7 +1241,7 @@ class StreamInfo(YouTubeRequestClient):
             data_cache.set_item(encrypted_signature, {'sig': signature})
 
         if signature:
-            url = '{0}&{1}={2}'.format(url, query_var, signature)
+            url = ''.join((url, '&', query_var, '=', signature))
             return url
         return None
 
@@ -1845,16 +1843,16 @@ class StreamInfo(YouTubeRequestClient):
                         role = 'alternate'
                         label = self._context.localize('stream.alternate')
 
-                    mime_group = '{0}_{1}.{2}'.format(
-                        mime_type, language_code, role_type
-                    )
+                    mime_group = ''.join((
+                        mime_type, '_', language_code, '.', role_type,
+                    ))
                     if language_code == self._language_base and (
                             not preferred_audio['id']
                             or role == 'main'
                             or role_type > preferred_audio['role_type']
                     ):
                         preferred_audio = {
-                            'id': '_{0}.{1}'.format(language_code, role_type),
+                            'id': ''.join(('_', language_code, '.', role_type)),
                             'language_code': language_code,
                             'role_type': role_type,
                         }
@@ -1870,9 +1868,10 @@ class StreamInfo(YouTubeRequestClient):
                 language = self._context.get_language_name(language_code)
                 label = '{0} ({1} kbps)'.format(label, bitrate // 1000)
                 if channels > 2 or 'auto' not in stream_select:
-                    quality_group = '{0}_{1}_{2}.{3}'.format(
-                        container, codec, language_code, role_type
-                    )
+                    quality_group = ''.join((
+                        container, '_', codec, '_', language_code,
+                        '.', role_type,
+                    ))
                 else:
                     quality_group = mime_group
             elif audio_only:
@@ -1937,7 +1936,7 @@ class StreamInfo(YouTubeRequestClient):
                     fps if fps > 30 else '',
                     ' HDR' if hdr else '',
                 )
-                quality_group = '{0}_{1}_{2}'.format(container, codec, label)
+                quality_group = '_'.join((container, codec, label))
 
             if mime_group not in data:
                 data[mime_group] = {}

resources/lib/youtube_plugin/kodion/constants/__init__.py
@@ -20,11 +20,11 @@ from . import (
 
 # Addon paths
 ADDON_ID = 'plugin.video.youtube'
-ADDON_PATH = 'special://home/addons/{id}'.format(id=ADDON_ID)
-DATA_PATH = 'special://profile/addon_data/{id}'.format(id=ADDON_ID)
+ADDON_PATH = 'special://home/addons/' + ADDON_ID
+DATA_PATH = 'special://profile/addon_data/' + ADDON_ID
 MEDIA_PATH = ADDON_PATH + '/resources/media'
 RESOURCE_PATH = ADDON_PATH + '/resources'
-TEMP_PATH = 'special://temp/{id}'.format(id=ADDON_ID)
+TEMP_PATH = 'special://temp/' + ADDON_ID
 
 # Const values
 VALUE_FROM_STR = {

resources/lib/youtube_plugin/kodion/monitors/player_monitor.py
@@ -266,7 +266,7 @@ class PlayerMonitorThread(threading.Thread):
                     if rating == 'none':
                         rating_match = re.search(
                             r'/(?P<video_id>[^/]+)/(?P<rating>[^/]+)',
-                            '/{0}/{1}/'.format(self.video_id, rating)
+                            '/'.join(('', self.video_id, rating, ''))
                         )
                         self._provider.on_video_x(
                             self._provider,

resources/lib/youtube_plugin/kodion/utils/methods.py
@@ -270,7 +270,7 @@ def get_kodi_setting_value(setting, process=None):
 
 
 def get_kodi_setting_bool(setting):
-    return xbmc.getCondVisibility('System.GetBool({0})'.format(setting))
+    return xbmc.getCondVisibility(setting.join(('System.GetBool(', ')')))
 
 
 def validate_ip_address(ip_address):

resources/lib/youtube_plugin/youtube/client/youtube.py
@@ -156,7 +156,7 @@ class YouTube(LoginClient):
             'Accept': '*/*',
             'Accept-Language': 'en-US,en;q=0.5',
             'DNT': '1',
-            'Referer': 'https://www.youtube.com/watch?v={0}'.format(video_id),
+            'Referer': 'https://www.youtube.com/watch?v=' + video_id,
             'User-Agent': ('Mozilla/5.0 (Linux; Android 10; SM-G981B)'
                            ' AppleWebKit/537.36 (KHTML, like Gecko)'
                            ' Chrome/80.0.3987.162 Mobile Safari/537.36'),
@@ -1976,7 +1976,7 @@ class YouTube(LoginClient):
             else:
                 notification = message
 
-            title = '{0}: {1}'.format(self._context.get_name(), reason)
+            title = ': '.join((self._context.get_name(), reason))
             if ok_dialog:
                 self._context.get_ui().on_ok(title, notification)
             else:

commit a235cfaa8bbc8cfe8d97ee74056b7c8fe589295b

Fix multi-audio streams failing to play
- Regression after cad487d

resources/lib/youtube_plugin/youtube/helper/stream_info.py
@@ -1820,8 +1820,8 @@ class StreamInfo(YouTubeRequestClient):
 
                     language = audio_track.get('id', default_lang_code)
                     if '.' in language:
-                        language_code, role_type = language.split('.')
-                        role_type = int(role_type)
+                        language_code, role_str = language.split('.')
+                        role_type = int(role_str)
                     else:
                         language_code = language
                         role_type = 4
@@ -1842,7 +1842,7 @@ class StreamInfo(YouTubeRequestClient):
                         label = self._context.localize('stream.alternate')
 
                     mime_group = ''.join((
-                        mime_type, '_', language_code, '.', role_type,
+                        mime_type, '_', language_code, '.', role_str,
                     ))
                     if language_code == self._language_base and (
                             not preferred_audio['id']
@@ -1850,7 +1850,7 @@ class StreamInfo(YouTubeRequestClient):
                             or role_type > preferred_audio['role_type']
                     ):
                         preferred_audio = {
-                            'id': ''.join(('_', language_code, '.', role_type)),
+                            'id': ''.join(('_', language_code, '.', role_str)),
                             'language_code': language_code,
                             'role_type': role_type,
                         }
@@ -1868,7 +1868,7 @@ class StreamInfo(YouTubeRequestClient):
                 if channels > 2 or 'auto' not in stream_select:
                     quality_group = ''.join((
                         container, '_', codec, '_', language_code,
-                        '.', role_type,
+                        '.', role_str,
                     ))
                 else:
                     quality_group = mime_group