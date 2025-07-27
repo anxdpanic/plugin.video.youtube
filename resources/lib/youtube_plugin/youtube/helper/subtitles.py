d522f8faa29f12328a83a541aef67e824175a423
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2025-07-27 13:12:46 +0900

    Update copyright notice dates

--- resources/lib/youtube_plugin/youtube/helper/subtitles.py
+++ resources/lib/youtube_plugin/youtube/helper/subtitles.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    Copyright (C) 2017-2021 plugin.video.youtube
+    Copyright (C) 2017-2025 plugin.video.youtube
 
     SPDX-License-Identifier: GPL-2.0-only
     See LICENSES/GPL-2.0-only for more information.
