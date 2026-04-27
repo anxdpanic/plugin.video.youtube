anxdpanic/plugin.video.youtube

commit 3b7955c9edc5af97470dd3ab0427fe9afe93a25e

Improve handling of retries upon server error #1438
- Don't raise an exception and limit retry attempts if manual redirect/fallback can be used instead

resources/lib/youtube_plugin/kodion/network/requests.py
@@ -62,6 +62,7 @@ class SSLHTTPAdapter(HTTPAdapter):
             _SSL_CONTEXT.load_verify_locations(capath=_CA_PATH)
         else:
             _SSL_CONTEXT.load_verify_locations(cafile=_CA_PATH)
+    MAX_TOTAL_RETRIES = 3
 
     def init_poolmanager(self, *args, **kwargs):
         kwargs['ssl_context'] = self._SSL_CONTEXT
@@ -82,6 +83,25 @@ class SSLHTTPAdapter(HTTPAdapter):
             conn.cert_reqs = str('CERT_NONE')
         conn.ca_certs = None
         conn.ca_cert_dir = None
+
+    def send(self, *args, **kwargs):
+        retry = self.max_retries
+        if kwargs.pop('_allow_redirects', True):
+            retry.total = self.MAX_TOTAL_RETRIES
+            retry.connect = None
+            retry.read = None
+            retry.redirect = None
+            retry.status = None
+            retry.other = None
+        else:
+            total = retry.total
+            retry.total = None
+            retry.connect = total
+            retry.read = total
+            retry.redirect = 0
+            retry.status = 1
+            retry.other = 0
+        return super(SSLHTTPAdapter, self).send(*args, **kwargs)
 
 
 class CustomSession(Session):
@@ -148,13 +168,20 @@ class CustomSession(Session):
             pool_maxsize=20,
             pool_block=True,
             max_retries=Retry(
-                total=3,
-                backoff_factor=0.1,
-                status_forcelist={500, 502, 503, 504},
+                total=SSLHTTPAdapter.MAX_TOTAL_RETRIES,
                 allowed_methods=None,
+                status_forcelist={500, 502, 503, 504},
+                backoff_factor=0.1,
+                raise_on_redirect=False,
+                raise_on_status=False,
+                respect_retry_after_header=True,
             )
         ))
         self.mount('http://', HTTPAdapter())
+
+    def send(self, *args, **kwargs):
+        kwargs['_allow_redirects'] = kwargs.get('allow_redirects', True)
+        return super(CustomSession, self).send(*args, **kwargs)
 
 
 class BaseRequestsClass(object):

commit f855429592f1c833651c1a19b62eb343808f3fca

Fix regressions with accessing http urls
- Caused by 3b7955c9edc5af97470dd3ab0427fe9afe93a25e
- Retries disabled
- Fix unexpected keyword argument

resources/lib/youtube_plugin/kodion/network/requests.py
@@ -14,7 +14,7 @@ from atexit import register as atexit_register
 from collections import OrderedDict
 from os.path import exists, isdir
 
-from requests.adapters import HTTPAdapter, Retry
+from requests.adapters import HTTPAdapter as _HTTPAdapter, Retry
 from requests.exceptions import InvalidJSONError, RequestException, URLRequired
 from requests.hooks import default_hooks
 from requests.models import DEFAULT_REDIRECT_LIMIT, Request
@@ -36,6 +36,31 @@ __all__ = (
     'BaseRequestsClass',
     'InvalidJSONError'
 )
+
+
+class HTTPAdapter(_HTTPAdapter):
+    MAX_TOTAL_RETRIES = 3
+
+    def send(self, *args, **kwargs):
+        retry = self.max_retries
+        num_retries = self.MAX_TOTAL_RETRIES
+        if kwargs.pop('_allow_redirects', True):
+            if retry.total is None:
+                retry.total = num_retries
+                retry.connect = None
+                retry.read = None
+                retry.redirect = None
+                retry.status = None
+                retry.other = None
+        else:
+            if retry.total:
+                retry.total = None
+                retry.connect = num_retries
+                retry.read = num_retries
+                retry.redirect = 0
+                retry.status = 0
+                retry.other = 0
+        return super(HTTPAdapter, self).send(*args, **kwargs)
 
 
 class SSLHTTPAdapter(HTTPAdapter):
@@ -62,7 +87,6 @@ class SSLHTTPAdapter(HTTPAdapter):
             _SSL_CONTEXT.load_verify_locations(capath=_CA_PATH)
         else:
             _SSL_CONTEXT.load_verify_locations(cafile=_CA_PATH)
-    MAX_TOTAL_RETRIES = 3
 
     def init_poolmanager(self, *args, **kwargs):
         kwargs['ssl_context'] = self._SSL_CONTEXT
@@ -83,25 +107,6 @@ class SSLHTTPAdapter(HTTPAdapter):
             conn.cert_reqs = str('CERT_NONE')
         conn.ca_certs = None
         conn.ca_cert_dir = None
-
-    def send(self, *args, **kwargs):
-        retry = self.max_retries
-        if kwargs.pop('_allow_redirects', True):
-            retry.total = self.MAX_TOTAL_RETRIES
-            retry.connect = None
-            retry.read = None
-            retry.redirect = None
-            retry.status = None
-            retry.other = None
-        else:
-            total = retry.total
-            retry.total = None
-            retry.connect = total
-            retry.read = total
-            retry.redirect = 0
-            retry.status = 1
-            retry.other = 0
-        return super(SSLHTTPAdapter, self).send(*args, **kwargs)
 
 
 class CustomSession(Session):